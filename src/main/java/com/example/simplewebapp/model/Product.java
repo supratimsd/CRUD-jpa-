f523482591035f985922c5ef0ba8aeb39ec14330
supratimsd <supratim.dey.21@aot.edu.in>
2024-08-04 00:18:21 +0530

    first commit

--- src/main/java/com/example/simplewebapp/model/Product.java
+++ src/main/java/com/example/simplewebapp/model/Product.java
@@ -2,18 +2,45 @@ package com.example.simplewebapp.model;
 
 // import jakarta.annotation.Nullable;
 
-// import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-@Data
-@AllArgsConstructor
-// @Component
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+
+
+@Component
+@Entity
 public class Product {
-    private Integer prodId;
+    @Id
+    private int prodId;
+    
     private String prodName;
-    private Integer prodPrice;
+    
+    private int prodPrice;
     
 
+    public int getProdId(){
+        return prodId;
+    }
+
+    public void setProdId(int prodId){
+        this.prodId=prodId;
+    }
+
+    public String getProdName(){
+        return prodName;
+    }
+
+    public void setProdName(String prodName){
+        this.prodName=prodName;
+    }
+
+    public int getProdPrice(){
+        return prodPrice;
+    }
+
+    public void setProdPrice(int prodPrice){
+        this.prodPrice=prodPrice;
+    }
 }
--- src/main/java/com/example/simplewebapp/service/ProductService.java
+++ src/main/java/com/example/simplewebapp/service/ProductService.java
@@ -1,49 +1,40 @@
 package com.example.simplewebapp.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-// import org.springframework.stereotype.Service;
 
-// import java.util.List;
 import com.example.simplewebapp.model.Product;
+import com.example.simplewebapp.repository.ProductRepo;
 
 @Service
 public class ProductService {
+    @Autowired
+    ProductRepo repo;
 
-    List<Product> products=new ArrayList<>(Arrays.asList(new Product(100,"A",500),new Product(101,"B",1000),new Product(102,"C",1500)));
+   
     public List<Product> getProducts(){
-        return products;
+        return repo.findAll();
     }
     public Product getProdId(int prodId) {
-        return products.stream().filter(p -> p.getProdId() == prodId).findFirst().get();
+        return repo.findById(prodId).orElse(new Product());
 
 
     }
 
     public void addProduct(Product prod){
-        products.add(prod);
+        repo.save(prod);
     }
 
     public void updateProduct(Product prod){
-        int index=0;
-        for(int i=0;i<products.size();i++){
-            if(products.get(i).getProdId()==prod.getProdId())
-                index=i;
-            products.set(index,prod);
-        }
+        repo.save(prod);
     }
     public void deleteProduct(int prodId) {
-        int index=0;
-        for(int i=0;i<products.size();i++)
-            if(products.get(i).getProdId()==prodId)
-                index=i;
-            products.remove(index);
-        
-       
+        repo.deleteById(prodId);
+
     }
 
 }
